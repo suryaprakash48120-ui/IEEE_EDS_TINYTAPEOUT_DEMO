8865b1f907bc9d8dc82d11dba270f0d60f632e4e
suryaprakash48120-ui <suryaprakash48120@gmail.com>
2026-09-16 12:01:30 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -8,19 +8,3 @@ module tt_um_half_adder (
     input  wire       clk,
     input  wire       rst_n
 );
-
-    // Half Adder
-    // ui_in[0] = A
-    // ui_in[1] = B
-    // uo_out[0] = SUM
-    // uo_out[1] = CARRY
-
-    assign uo_out[0] = ui_in[0] ^ ui_in[1];
-    assign uo_out[1] = ui_in[0] & ui_in[1];
-
-    assign uo_out[7:2] = 6'b0;
-
-    assign uio_out = 8'b0;
-    assign uio_oe  = 8'b0;
-
-endmodule
