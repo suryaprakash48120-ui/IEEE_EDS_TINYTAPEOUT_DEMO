de495f611013b8285df8a8df6b7c85549a7a1493
suryaprakash48120-ui <suryaprakash48120@gmail.com>
2026-09-16 11:47:39 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -5,43 +5,43 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
+
     dut._log.info("Start")
 
     # Start clock
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
+    # Enable
     dut.ena.value = 1
-    dut.ui_in.value = 0
+
+    # Reset
+    dut.ui.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
 
     await ClockCycles(dut.clk, 2)
+
     dut.rst_n.value = 1
 
     # Test 00
-    dut.ui_in.value = 0b00
+    dut.ui.value = 0b00
     await ClockCycles(dut.clk, 1)
-
-    assert dut.uo_out.value & 0b11 == 0b00
+    assert (dut.uo.value & 0b11) == 0b00
 
     # Test 01
-    dut.ui_in.value = 0b01
+    dut.ui.value = 0b01
     await ClockCycles(dut.clk, 1)
-
-    assert dut.uo_out.value & 0b11 == 0b01
+    assert (dut.uo.value & 0b11) == 0b01
 
     # Test 10
-    dut.ui_in.value = 0b10
+    dut.ui.value = 0b10
     await ClockCycles(dut.clk, 1)
-
-    assert dut.uo_out.value & 0b11 == 0b01
+    assert (dut.uo.value & 0b11) == 0b01
 
     # Test 11
-    dut.ui_in.value = 0b11
+    dut.ui.value = 0b11
     await ClockCycles(dut.clk, 1)
+    assert (dut.uo.value & 0b11) == 0b10
 
-    assert dut.uo_out.value & 0b11 == 0b10
-
-    dut._log.info("Half Adder test completed successfully!")
+    dut._log.info("HALF ADDER TEST PASSED")
