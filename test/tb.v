b50b34c5902ec11f7560e24079dc1e5cd318ca44
suryaprakash48120-ui <suryaprakash48120@gmail.com>
2026-09-16 12:00:57 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,18 +1,46 @@
-// Replace tt_um_example with your module name:
-tt_um_half_adder uut (
+`default_nettype none
+`timescale 1ns / 1ps
 
-    // Include power ports for the Gate Level test:
+module tb ();
+
+    // Dump signals to FST file
+    initial begin
+        $dumpfile("tb.fst");
+        $dumpvars(0, tb);
+        #1;
+    end
+
+    // Inputs
+    reg clk;
+    reg rst_n;
+    reg ena;
+    reg [7:0] ui_in;
+    reg [7:0] uio_in;
+
+    // Outputs
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
+
 `ifdef GL_TEST
-    .VPWR(VPWR),
-    .VGND(VGND),
+    wire VPWR = 1'b1;
+    wire VGND = 1'b0;
 `endif
 
-    .ui_in  (ui_in),
-    .uo_out (uo_out),
-    .uio_in (uio_in),
-    .uio_out(uio_out),
-    .uio_oe (uio_oe),
-    .ena    (ena),
-    .clk    (clk),
-    .rst_n  (rst_n)
-);
+    // Instantiate Half Adder
+    tt_um_half_adder uut (
+`ifdef GL_TEST
+        .VPWR(VPWR),
+        .VGND(VGND),
+`endif
+        .ui_in(ui_in),
+        .uo_out(uo_out),
+        .uio_in(uio_in),
+        .uio_out(uio_out),
+        .uio_oe(uio_oe),
+        .ena(ena),
+        .clk(clk),
+        .rst_n(rst_n)
+    );
+
+endmodule
