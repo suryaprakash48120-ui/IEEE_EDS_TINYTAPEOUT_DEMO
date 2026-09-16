95f0dcfc7498d140930634960b5e8cbdaed53166
suryaprakash48120-ui <suryaprakash48120@gmail.com>
2026-09-16 12:13:55 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -12,17 +12,18 @@ module tt_um_half_adder (
     // Half Adder
     // ui_in[0] = A
     // ui_in[1] = B
-    // uo_out[0] = SUM
-    // uo_out[1] = CARRY
 
+    // Sum = A XOR B
     assign uo_out[0] = ui_in[0] ^ ui_in[1];
+
+    // Carry = A AND B
     assign uo_out[1] = ui_in[0] & ui_in[1];
 
-    // Unused outputs
-    assign uo_out[7:2] = 6'b0;
+    // Unused output pins
+    assign uo_out[7:2] = 6'b000000;
 
     // Bidirectional pins unused
-    assign uio_out = 8'b0;
-    assign uio_oe  = 8'b0;
+    assign uio_out = 8'b00000000;
+    assign uio_oe  = 8'b00000000;
 
 endmodule
